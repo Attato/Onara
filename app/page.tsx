ad0bd1aaaa378c247aea75f55685609124198dfc
KoffeMeow <guyden999@mail.ru>
2023-03-18 22:42:25 +0300

    initial filling with content

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
 import Image from 'next/image';
+import Head from 'next/head';
+
+import Header from '@/components/header/header';
+
 import styles from './page.module.scss';
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<div className={styles.masthead}>
-				<Image src="/logo.svg" width={524} height={104} alt="img"></Image>
-			</div>
-		</main>
+		<div className={styles.page_container}>
+			<Head>
+				<link rel="icon" href="/icon.svg" />
+			</Head>
+			<Header />
+			<main className={styles.main}>
+				<div className={styles.masthead}>
+					<Image src="/logo.svg" width={264} height={54} alt="img"></Image>
+				</div>
+			</main>
+		</div>
 	);
 }
