5784e4d3cff71f5753f01f03ace699c5cf679862
KoffeMeow <guyden999@mail.ru>
2023-03-21 00:07:13 +0300

    use layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
 import Image from 'next/image';
-import Head from 'next/head';
-
-import Header from '@/components/header/header';
 
 import styles from './page.module.scss';
 
 export default function Home() {
 	return (
-		<div className={styles.page_container}>
-			<Head>
-				<link rel="icon" href="/icon.svg" />
-			</Head>
-			<Header />
-			<main className={styles.main}>
-				<div className={styles.masthead}>
-					<Image src="/logo.svg" width={264} height={54} alt="img"></Image>
-				</div>
-			</main>
-		</div>
+		<main className="main">
+			<div className={styles.masthead}>
+				<Image src="/logo.svg" width={264} height={54} alt="img"></Image>
+			</div>
+		</main>
 	);
 }
