f27476d6c7320ab6e416480bca24235c2dd059f8
KoffeMeow <guyden999@mail.ru>
2023-03-29 01:05:06 +0300

    login page update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,54 @@
+'use client';
+
+import { useState } from 'react';
+
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 import styles from './page.module.scss';
 
-const Help: NextPage = () => {
+const Login: NextPage = () => {
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = (event: any) => {
+		event.preventDefault();
+		// handle form submission
+	};
+
 	return (
 		<main className="main">
-			<h1>Welcome to Onara! </h1>
+			<form className={styles.form} onSubmit={handleSubmit}>
+				<h1>Log in to Onara</h1>
+				<label>
+					Username:
+					<input
+						type="text"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+				</label>
+				<label>
+					Email:
+					<input
+						type="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</label>
+				<label>
+					Password:
+					<input
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</label>
+				<button type="submit">Continue with Email</button>
+			</form>
 		</main>
 	);
 };
 
-export default Help;
+export default Login;
