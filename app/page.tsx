89a3e39f5b84bf8ea91a8f61abcefdf8f0b479d5
KoffeMeow <guyden999@mail.ru>
2023-03-29 12:33:36 +0300

    page preparation for future implementation

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,20 @@ const Home: NextPage = () => {
 	return (
 		<main className="main">
 			<div className={styles.masthead}>
-				<Image src="/logo.svg" width={264} height={54} alt="img"></Image>
-				<Image src="/bg1.png" width={512} height={512} alt="img"></Image>
+				<h1>IMAGINE A PLACE...</h1>
+				<p>... *text*</p>
+				{/* <Image src="/logo.svg" width={264} height={54} alt="img" /> */}
 			</div>
 
-			<div className={styles.empty} />
+			<div className={styles.empty}>
+				<Image
+					src="/bg1.png"
+					width={512}
+					height={512}
+					alt="img"
+					className={styles.cosmonaut}
+				/>
+			</div>
 		</main>
 	);
 };
