4257f3f9802e973d8919ac8797b783006f276d83
KoffeMeow <guyden999@mail.ru>
2023-04-03 03:29:26 +0300

    createe Get stated page

---
+++ app/docs/get-started/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import type { NextPage } from 'next';
+import Link from 'next/link';
+
+import { usePathname, useRouter } from 'next/navigation';
+
+import styles from './page.module.scss';
+
+const Docs: NextPage = () => {
+	return (
+		<div className="page_content">
+			<h1>Getting Started</h1>
+
+			<div className={styles.column}>
+				<h2 id="step-1">Step 1</h2>
+				<h2 id="step-2">Step 2</h2>
+				<h2 id="step-3">Step 3</h2>
+				<h2 id="step-4">Step 4</h2>
+				<h2 id="step-5">Step 5</h2>
+			</div>
+		</div>
+	);
+};
+
+export default Docs;
