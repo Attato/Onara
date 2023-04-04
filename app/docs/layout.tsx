ec13c2b3c2336e7d1abbe28ed60cfea1b4ea7373
Attato <rde8519@mail.ru>
2023-04-04 17:31:43 +0300

    checking block overflow and fixing links

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -6,17 +6,36 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
 		<div className={styles.slug}>
 			<div className={styles.sidebar}>
 				<Link href="/docs">Introduction</Link>
+
 				<section>
 					<Link href="/docs/get-started">Get started</Link>
 					<div className={styles.links}>
-						<Link href="/docs/get-started#step-1">Step 1 - *text*</Link>
-						<Link href="/docs/get-started#step-2">Step 2 - *text*</Link>
-						<Link href="/docs/get-started#step-3">Step 3 - *text*</Link>
-						<Link href="/docs/get-started#step-4">Step 4 - *text*</Link>
-						<Link href="/docs/get-started#step-5">Step 5 - *text*</Link>
+						<Link href="#step-1">Step 1 - *text*</Link>
+						<Link href="#step-2">Step 2 - *text*</Link>
+						<Link href="#step-3">Step 3 - *text*</Link>
+						<Link href="#step-4">Step 4 - *text*</Link>
+						<Link href="#step-5">Step 5 - *text*</Link>
 					</div>
 				</section>
+
 				<Link href="/docs/guide">Guide</Link>
+
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
+				<Link href="/docs">There will be something here soon</Link>
 			</div>
 			{children}
 		</div>
