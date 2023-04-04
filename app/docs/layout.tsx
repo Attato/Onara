e110fe7f0a662e2c60652dfb42cc3d2cb4712f3a
Attato <rde8519@mail.ru>
2023-04-04 14:53:21 +0300

    change classname

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,28 +1,26 @@
 import Link from 'next/link';
 import styles from './layout.module.scss';
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<main className="main">
-			<div className={styles.gluing}>
-				<div className={styles.sidebar}>
-					<Link href="/docs">Introduction</Link>
-					<section>
-						<Link href="/docs/get-started">Get started</Link>
-						<div className={styles.links}>
-							<Link href="/docs/get-started#step-1">Step 1 - *text*</Link>
-							<Link href="/docs/get-started#step-2">Step 2 - *text*</Link>
-							<Link href="/docs/get-started#step-3">Step 3 - *text*</Link>
-							<Link href="/docs/get-started#step-4">Step 4 - *text*</Link>
-							<Link href="/docs/get-started#step-5">Step 5 - *text*</Link>
-						</div>
-					</section>
-					<Link href="/docs/guide">Guide</Link>
-				</div>
-				{children}
+const Layout = ({ children }: { children: React.ReactNode }) => (
+	<main className="main">
+		<div className={styles.slug}>
+			<div className={styles.sidebar}>
+				<Link href="/docs">Introduction</Link>
+				<section>
+					<Link href="/docs/get-started">Get started</Link>
+					<div className={styles.links}>
+						<Link href="/docs/get-started#step-1">Step 1 - *text*</Link>
+						<Link href="/docs/get-started#step-2">Step 2 - *text*</Link>
+						<Link href="/docs/get-started#step-3">Step 3 - *text*</Link>
+						<Link href="/docs/get-started#step-4">Step 4 - *text*</Link>
+						<Link href="/docs/get-started#step-5">Step 5 - *text*</Link>
+					</div>
+				</section>
+				<Link href="/docs/guide">Guide</Link>
 			</div>
-		</main>
-	);
-};
+			{children}
+		</div>
+	</main>
+);
 
 export default Layout;
