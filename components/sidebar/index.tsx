86696ed21c46924638b99c9dd214037e68d83029
KoffeMeow <guyden999@mail.ru>
2023-04-18 18:11:31 +0300

    added sidebar component for documentation pages

---
+++ components/sidebar/index.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import styles from './index.module.scss';
+
+const SideBar = ({ posts }: any) => {
+	const pathname = usePathname();
+
+	return (
+		<div className={styles.sidebar}>
+			{posts
+				.sort((a: any, b: any) => a.frontMatter.id - b.frontMatter.id)
+				.map((link: any, id: number) => {
+					const isActive = pathname === `/docs/${link.slug}`;
+
+					return (
+						<Link
+							href={link.slug}
+							key={id}
+							className={isActive ? styles.activeLink : ''}
+							draggable={false}
+						>
+							{link.frontMatter.title}
+						</Link>
+					);
+				})}
+		</div>
+	);
+};
+
+export default SideBar;
