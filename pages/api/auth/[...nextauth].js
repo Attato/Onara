a6e297c4eafd533aee2b4631049a82314e5e3e63
KoffeMeow <guyden999@mail.ru>
2023-04-22 01:06:10 +0300

    change process.env variables

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,12 +4,8 @@ import GithubProvider from 'next-auth/providers/github';
 export default NextAuth({
 	providers: [
 		GithubProvider({
-			clientId: process.env.GITHUB_CLIENT_ID,
-			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+			clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+			clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
 		}),
-		// Providers.GitLab({
-		// 	clientId: process.env.GITLAB_CLIENT_ID,
-		// 	clientSecret: process.env.GITLAB_CLIENT_SECRET,
-		// }),
 	],
 });
