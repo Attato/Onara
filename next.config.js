6e2c33105d2caecd05462e20e10e6f51c2cf09ee
KoffeMeow <guyden999@mail.ru>
2023-04-25 13:30:31 +0300

    added the ability to get images of user avatars from github

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
 		appDir: true,
 		typedRoutes: true,
 	},
+
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com',
+				port: '',
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
