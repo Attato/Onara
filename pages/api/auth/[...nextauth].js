8d6d1c9014c0ff04f85024c99c90f8ddf909830f
KoffeMeow <guyden999@mail.ru>
2023-05-17 23:48:03 +0300

    add gitlab

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,16 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GitlabProvider from 'next-auth/providers/gitlab';
 
 export default NextAuth({
 	providers: [
 		GithubProvider({
 			clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
 			clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
+		}),
+		GitlabProvider({
+			clientId: process.env.NEXT_PUBLIC_GITLAB_CLIENT_ID,
+			clientSecret: process.env.NEXT_PUBLIC_GITLAB_CLIENT_SECRET,
 		}),
 	],
 });
