4f11083e76fc249a805bbd281ef41c3c96bd6861
KoffeMeow <guyden999@mail.ru>
2023-05-17 23:49:38 +0300

    decomposed

---
+++ components/Footer/links.tsx
@@ -0,0 +1,34 @@
+const productLinks = [
+	{ href: '/docs/release-notes', label: 'Release Notes' },
+	{ href: '/docs/security', label: 'Security' },
+	{ href: '/', label: 'Sitemap' },
+	{ href: '/', label: 'What is Onara?' },
+];
+
+const resourcesLinks = [
+	{ href: '/docs/introduction', label: 'Documentation' },
+	{ href: '/docs/user-guide', label: 'User Guide' },
+	{ href: '/help', label: 'Help' },
+	{ href: '/', label: 'Open Source Software' },
+];
+
+const companyLinks = [
+	{ href: '/', label: 'Home' },
+	{ href: '/about', label: 'About' },
+	{ href: '/feedback', label: 'Feedback' },
+	{ href: '/', label: 'Contact Us' },
+];
+
+const policyLinks = [
+	{ href: '/', label: 'Privacy Policy' },
+	{ href: '/', label: 'Terms of Service' },
+	{ href: '/', label: 'Cookie Preferences' },
+	{ href: '/docs/legal-licensing', label: 'License' },
+];
+
+export const footerLinks = [
+	{ title: 'Product', links: productLinks },
+	{ title: 'Resources', links: resourcesLinks },
+	{ title: 'Company', links: companyLinks },
+	{ title: 'Policies', links: policyLinks },
+];
