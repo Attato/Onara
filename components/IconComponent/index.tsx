1c6848dae772cb4aa6d4c266fdc28714abb03b04
Attato <rde8519@mail.ru>
2023-05-18 18:14:16 +0300

    add fill

--- components/IconComponent/index.tsx
+++ components/IconComponent/index.tsx
@@ -7,6 +7,7 @@ type IconComponentProps = {
 	width?: number;
 	height?: number;
 	strokeWidth?: number;
+	fill?: string;
 	children: ReactNode;
 };
 
@@ -14,12 +15,13 @@ const IconComponent = ({
 	width = 16,
 	height = 16,
 	strokeWidth = 2,
+	fill = 'none',
 	children,
 }: IconComponentProps) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
+			fill={fill}
 			viewBox="0 0 24 24"
 			strokeWidth={strokeWidth}
 			stroke="currentColor"
