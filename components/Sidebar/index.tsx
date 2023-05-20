0eff9a9f92c87accad2630f92c455e1642b1e579
KoffeMeow <guyden999@mail.ru>
2023-05-21 01:07:12 +0300

    changed style, added categories

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,31 +1,47 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-
 import styles from './index.module.scss';
+import { Post } from '@/pages/docs/[slug]';
 
-const SideBar = ({ posts }: any) => {
+interface SidebarProps {
+	posts: Post[];
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ posts }) => {
 	const pathname = usePathname();
+
+	const sortedPosts = [...posts].sort(
+		(a, b) => a.frontMatter.id - b.frontMatter.id
+	);
+
+	const categories = sortedPosts.reduce((categories, post) => {
+		const category = post.frontMatter.category;
+		const isActive = pathname === `/docs/${post.slug}`;
+
+		categories[category] = categories[category] || [];
+		categories[category].push(
+			<Link
+				key={post.slug}
+				href={`/docs/${post.slug}`}
+				className={isActive ? styles.activeLink : ''}
+			>
+				{post.frontMatter.title}
+			</Link>
+		);
+
+		return categories;
+	}, {} as { [category: string]: JSX.Element[] });
 
 	return (
 		<div className={styles.sidebar}>
-			{posts
-				.sort((a: any, b: any) => a.frontMatter.id - b.frontMatter.id)
-				.map((link: any, id: number) => {
-					const isActive = pathname === `/docs/${link.slug}`;
-
-					return (
-						<Link
-							href={link.slug}
-							key={id}
-							className={isActive ? styles.activeLink : ''}
-							draggable={false}
-						>
-							{link.frontMatter.title}
-						</Link>
-					);
-				})}
+			{Object.entries(categories).map(([category, links]) => (
+				<div key={category} className={styles.category}>
+					<h4>{category}</h4>
+					{links}
+				</div>
+			))}
 		</div>
 	);
 };
 
-export default SideBar;
+export default Sidebar;
