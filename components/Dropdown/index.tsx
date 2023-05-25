27c2e0f2470ce17a91faf9c5c4af50cf1b966616
KoffeMeow <guyden999@mail.ru>
2023-05-25 23:54:33 +0300

    rethought dropdown, remade it for integration into the footer

--- components/Dropdown/index.tsx
+++ components/Dropdown/index.tsx
@@ -1,69 +1,58 @@
-import { useState, useEffect, useRef, ReactNode } from 'react';
+import React, { useState } from 'react';
 
 import { motion, AnimatePresence } from 'framer-motion';
+
+import Link from 'next/link';
 
 import IconWrapper from '@/components/IconWrapper';
 
 import styles from './index.module.scss';
 
-interface DropdownProps {
-	children: ReactNode;
-	label: ReactNode;
-}
+type DropdownProps = {
+	title?: React.ReactNode;
+	buttonContent: React.ReactNode;
+	children: React.ReactNode;
+	positionAbsolute?: boolean;
+};
 
-const Dropdown: React.FC<DropdownProps> = ({ children, label }) => {
+const Dropdown: React.FC<DropdownProps> = ({
+	buttonContent,
+	children,
+	positionAbsolute = false,
+}) => {
 	const [isOpen, setIsOpen] = useState(false);
-	const dropdownRef = useRef<HTMLDivElement>(null);
 
-	const handleOutsideClick = (event: MouseEvent) => {
-		if (
-			dropdownRef.current &&
-			!dropdownRef.current.contains(event.target as Node)
-		) {
-			setIsOpen(false);
-		}
-	};
-
-	useEffect(() => {
-		document.addEventListener('click', handleOutsideClick);
-		return () => {
-			document.removeEventListener('click', handleOutsideClick);
-		};
-	}, []);
-
-	const handleDropdownToggle = () => {
+	const toggleDropdown = () => {
 		setIsOpen(!isOpen);
 	};
 
 	return (
-		<div className={styles.dropdown} ref={dropdownRef}>
-			<button
-				onClick={handleDropdownToggle}
-				type="button"
-				className={styles.dropdown_btn}
-				aria-haspopup="true"
-				aria-expanded="true"
-			>
-				{label}
-				<IconWrapper width={14} height={14}>
-					<path
+		<div className={styles.dropdown}>
+			<motion.button onClick={toggleDropdown} className={styles.dropdown_btn}>
+				{buttonContent}
+
+				<IconWrapper>
+					<motion.path
 						strokeLinecap="round"
 						strokeLinejoin="round"
 						d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+						animate={{
+							rotate: isOpen ? 180 : 0,
+						}}
+						transition={{ duration: 0.3 }}
 					/>
 				</IconWrapper>
-			</button>
+			</motion.button>
 
 			<AnimatePresence>
 				{isOpen && (
 					<motion.div
+						initial={{ opacity: 0, height: 0 }}
+						animate={{ opacity: 1, height: 'auto' }}
+						exit={{ opacity: 0, height: 0 }}
+						transition={{ duration: 0.3 }}
 						className={styles.dropdown_open}
-						role="menu"
-						aria-orientation="vertical"
-						aria-labelledby="options-menu"
-						initial={{ opacity: 0, y: -20 }}
-						animate={{ opacity: 1, y: 0 }}
-						exit={{ opacity: 0, y: -20 }}
+						style={positionAbsolute ? { position: 'absolute' } : undefined}
 					>
 						{children}
 					</motion.div>
