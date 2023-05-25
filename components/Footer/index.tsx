730becacdd33b685b8ef677e550fb4770e1a36a0
KoffeMeow <guyden999@mail.ru>
2023-05-26 01:27:55 +0300

    delete useless code

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,35 +1,15 @@
 import React, { FC } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import Link from 'next/link';
 
-import IconWrapper from '@/components/IconWrapper';
-import Dropdown from '@/components/Dropdown'; // Import the Dropdown component
-
 import useScrollToTop from '@/hooks/useScrollToTop';
-
-import { footerLinks } from '@/data/components/footer/links';
 
 import styles from './index.module.scss';
 
 const Footer: FC = () => {
 	return (
 		<footer className={styles.footer}>
-			<nav>
-				{footerLinks.map((group, index) => (
-					<div className={styles.footer_group} key={index}>
-						<Dropdown buttonContent={group.title}>
-							<div className={styles.footer_list}>
-								{group.links.map((link, index) => (
-									<Link key={index} href={link.href}>
-										{link.label}
-									</Link>
-								))}
-							</div>
-						</Dropdown>
-					</div>
-				))}
-			</nav>
 			<div className={styles.footer_wrapper}>
 				<div className={styles.logo}>
 					<Link href="/" onClick={useScrollToTop()} className={styles.logo}>
