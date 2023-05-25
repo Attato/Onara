44e53d4d06fbc3e8c595772ed716b1b358c13b21
KoffeMeow <guyden999@mail.ru>
2023-05-26 00:04:23 +0300

    I removed the extra code due to the fact that the popup logic is now inside the component, added a link to the patreon, changed the style of the language selection button

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,16 +16,6 @@ import { stacks } from '../data/pages/home/stack';
 import styles from './index.module.scss';
 
 const Home: NextPage = () => {
-	const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
-
-	const openPopup = () => {
-		setIsPopupOpen(!isPopupOpen);
-	};
-
-	const closePopup = () => {
-		setIsPopupOpen(false);
-	};
-
 	return (
 		<>
 			<Head>
@@ -58,21 +48,21 @@ const Home: NextPage = () => {
 								/>
 							</IconWrapper>
 						</button>
-						<button onClick={openPopup} id={styles.get_started_btn}>
-							Get Started
-							<IconWrapper>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
-								/>
-							</IconWrapper>
-						</button>
 
 						<AuthorizationPopup
-							isPopupOpen={isPopupOpen}
-							popupOnClose={closePopup}
 							title="Log in"
+							buttonContent={
+								<button id={styles.get_started_btn}>
+									Get Started
+									<IconWrapper>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
+										/>
+									</IconWrapper>
+								</button>
+							}
 						/>
 					</div>
 				</motion.div>
@@ -82,6 +72,7 @@ const Home: NextPage = () => {
 						width={512}
 						height={512}
 						alt="Onara - Rabbit Astronaut"
+						priority={true}
 					></Image>
 				</div>
 			</motion.main>
@@ -139,7 +130,7 @@ const Home: NextPage = () => {
 						enjoys using our product, your input will help make Onara better.
 					</p>
 
-					<button onClick={() => alert('This button is not working yet')}>
+					<Link href="https://www.patreon.com/user?u=93866410">
 						Sponsor the creator
 						<IconWrapper width={14} height={14} fill="var(--warning)">
 							<path
@@ -148,7 +139,7 @@ const Home: NextPage = () => {
 								d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
 							/>
 						</IconWrapper>
-					</button>
+					</Link>
 				</div>
 			</div>
 
