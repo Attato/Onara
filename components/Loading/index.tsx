500c2f231c14b0da7827a8929e2ea58b24849fd9
KoffeMeow <guyden999@mail.ru>
2023-05-25 23:58:29 +0300

    I rewrote it to a typescript, added optional logic and a new color parameter

--- components/Loading/index.tsx
+++ components/Loading/index.tsx
@@ -1,11 +1,25 @@
 import styles from './index.module.scss';
 
-const Loading = ({ width, height }: any) => {
+type LoadingProps = {
+	width?: number;
+	height?: number;
+	color?: string;
+};
+
+const Loading: React.FC<LoadingProps> = ({
+	width = 24,
+	height = 24,
+	color = '#0082f5',
+}) => {
 	return (
 		<div className={styles.spinner_container}>
 			<div
 				className={styles.spinner}
-				style={{ width: `${width}px`, height: `${height}px` }}
+				style={{
+					width: `${width}px`,
+					height: `${height}px`,
+					borderTopColor: `${color}`,
+				}}
 			></div>
 		</div>
 	);
