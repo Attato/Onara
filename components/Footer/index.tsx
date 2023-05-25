9ffc2794391842f8bda7423c6f87b00b9fa15a98
KoffeMeow <guyden999@mail.ru>
2023-05-25 23:55:17 +0300

    corrected styles, integrated dropdown

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,10 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 import Link from 'next/link';
 
 import IconWrapper from '@/components/IconWrapper';
+import Dropdown from '@/components/Dropdown'; // Import the Dropdown component
 
 import useScrollToTop from '@/hooks/useScrollToTop';
 
@@ -12,57 +13,20 @@ import { footerLinks } from '@/data/components/footer/links';
 import styles from './index.module.scss';
 
 const Footer: FC = () => {
-	const [openSections, setOpenSections] = useState<number[]>([]);
-
-	const toggleSection = (index: number) => {
-		if (openSections.includes(index)) {
-			setOpenSections(
-				openSections.filter((sectionIndex) => sectionIndex !== index)
-			);
-		} else {
-			setOpenSections([...openSections, index]);
-		}
-	};
-
 	return (
 		<footer className={styles.footer}>
 			<nav>
 				{footerLinks.map((group, index) => (
-					<div className={styles.footer_group} key={group.title}>
-						<motion.button onClick={() => toggleSection(index)}>
-							{group.title}
-							<IconWrapper>
-								{/* Apply the rotation animation to the path */}
-								<motion.path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-									animate={{
-										rotate: openSections.includes(index) ? 180 : 0,
-									}}
-									transition={{ duration: 0.3 }}
-								/>
-							</IconWrapper>
-						</motion.button>
-
-						<AnimatePresence>
-							{openSections.includes(index) && (
-								<motion.div
-									initial={{ opacity: 0, height: 0 }}
-									animate={{ opacity: 1, height: 'auto' }}
-									exit={{ opacity: 0, height: 0 }}
-									transition={{ duration: 0.3 }}
-									className={styles.footer_list}
-									style={{ overflow: 'hidden' }}
-								>
-									{group.links.map(({ href, label }) => (
-										<Link href={href} key={label}>
-											{label}
-										</Link>
-									))}
-								</motion.div>
-							)}
-						</AnimatePresence>
+					<div className={styles.footer_group} key={index}>
+						<Dropdown buttonContent={group.title}>
+							<div className={styles.footer_list}>
+								{group.links.map((link, index) => (
+									<Link key={index} href={link.href}>
+										{link.label}
+									</Link>
+								))}
+							</div>
+						</Dropdown>
 					</div>
 				))}
 			</nav>
