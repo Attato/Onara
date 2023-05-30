2ea6a69f953ca64bbc752068cc2ff760611bbc91
KoffeMeow <guyden999@mail.ru>
2023-05-30 23:53:53 +0300

    update links

--- data/components/header/links.tsx
+++ data/components/header/links.tsx
@@ -1,9 +1,14 @@
 export const links = [
-	{ href: '/features', label: 'Features' },
-	{ href: '/changelog', label: 'Changelog' },
-	{ href: '/docs/introduction', label: 'Docs' },
+	{ href: '/changelog', label: 'Changelog', target: '_self' },
+	{ href: '/docs/introduction', label: 'Docs', target: '_self' },
+	{
+		href: 'https://www.patreon.com/Attato',
+		label: 'Support',
+		target: '_blank',
+	},
 	{
 		href: 'https://github.com/Attato/Onara/discussions/categories/feedback',
 		label: 'Feedback',
+		target: '_blank',
 	},
 ];
