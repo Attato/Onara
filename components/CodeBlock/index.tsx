f67535fe10fb50c8d51e6cdc3a73726c86e36b6d
KoffeMeow <guyden999@mail.ru>
2023-05-30 23:53:00 +0300

    + showLineNumbers

--- components/CodeBlock/index.tsx
+++ components/CodeBlock/index.tsx
@@ -9,11 +9,13 @@ import styles from './index.module.scss';
 type CodeBlockProps = {
 	language?: string;
 	code?: string;
+	showLineNumbers?: boolean;
 };
 
 const CodeBlock: React.FC<CodeBlockProps> = ({
 	language = 'md',
 	code = 'The majestic empty block of code is a lonely relic in the bottomless vastness of programming, resembling a forgotten and lost underground temple. Like a magnificent obelisk, it stretches to the heights of heaven, surrounded by dense shadows, as if dark secrets and forgotten magic spells are hidden inside it. Huge symbols and mystical comments engraved on the surface of the block illustrate the eternal battle between the programmer and errors, between light and darkness. The code symbols, like ancient runes, seal the power inside this magical space waiting to be filled with new instructions.',
+	showLineNumbers = true,
 }) => {
 	const [copySuccess, setCopySuccess] = useState(false);
 
@@ -45,7 +47,7 @@ const CodeBlock: React.FC<CodeBlockProps> = ({
 			<SyntaxHighlighter
 				language={language}
 				style={oneDark}
-				showLineNumbers={true}
+				showLineNumbers={showLineNumbers}
 			>
 				{code}
 			</SyntaxHighlighter>
