d8c79c889fb0cc26bf19cca4a1f838197d93b5c3
KoffeMeow <guyden999@mail.ru>
2023-06-01 22:47:51 +0300

    Moved the sidebar to a separate component, added logic providing for a request limit in the github api

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -9,31 +9,24 @@ import { GetServerSidePropsContext } from 'next';
 
 import {
 	MapPinIcon,
-	UsersIcon,
 	ArrowTopRightOnSquareIcon,
+	UsersIcon,
 } from '@heroicons/react/24/outline';
-import { links } from '@/data/pages/profile/links';
+
+import Alert from '@/components/Alert';
+import Sidebar from '@/components/Sidebar';
 
 import styles from './index.module.scss';
 
-interface ProfileProps {
+export interface ProfileProps {
 	profileData: any;
-	languageUsage: LanguageUsage[];
 }
 
-interface LanguageUsage {
-	language: string;
-	count: number;
-	percentage: number;
-}
-
-interface Repo {
-	language: string;
-}
-
-const Profile: NextPage<ProfileProps> = ({ profileData, languageUsage }) => {
+const Profile: NextPage<ProfileProps> = ({ profileData }) => {
 	const { status } = useSession();
-	const [profile, setProfile] = useState(profileData);
+	const [profile, setProfile] = useState<any>(profileData);
+
+	console.log(profile);
 
 	useEffect(() => {
 		setProfile(profileData);
@@ -42,7 +35,7 @@ const Profile: NextPage<ProfileProps> = ({ profileData, languageUsage }) => {
 	return (
 		<>
 			<Head>
-				<title>Onara</title>
+				<title>{profile?.name}</title>
 				<meta
 					name="description"
 					content="Onara is the perfect way to administer your repositories. Administer your application easily and efficiently."
@@ -53,66 +46,63 @@ const Profile: NextPage<ProfileProps> = ({ profileData, languageUsage }) => {
 			{status === 'authenticated' && (
 				<div className={styles.profile}>
 					<div className={styles.profile_content}>
-						<div className={styles.profile_sidebar}>
-							{links.map((link) => (
-								<Link href={link.href} className={styles.link} key={link.label}>
-									{link.label}
-									{link.image}
-								</Link>
-							))}
-						</div>
+						<Sidebar username={profile?.name} />
+
 						<div className={styles.landing}>
 							<div className={styles.user_details}>
-								<Image
-									src={`${profile?.avatar_url}`}
-									width={150}
-									height={150}
-									alt="profile image"
-									priority={true}
-								/>
-								<div className={styles.user_info}>
-									<h1>{profile?.name}</h1>
-									<span className={styles.description}>{profile.bio}</span>
-									<div className={styles.user_info_rows}>
-										<div className={styles.row}>
-											<MapPinIcon width={16} height={16} />
-											{profile.location}
-										</div>
-										<div className={styles.row}>
-											<UsersIcon width={16} height={16} />
-											<Link href={profile.followers_url}>
-												{profile.followers} followers
-											</Link>
-											<Link href={profile.following_url}>
-												{profile.following} following
-											</Link>
-										</div>
-									</div>
-									<Link
-										className={styles.github_link}
-										href={profile.html_url}
-										target="_blank"
-									>
-										Go to GitHub profile
-										<ArrowTopRightOnSquareIcon width={14} height={14} />
-									</Link>
-								</div>
-							</div>
+								{!profile && (
+									<Alert text="You have exceeded the traffic limit, you will have to wait an hour." />
+								)}
 
-							<div className={styles.languages}>
-								<h2>Most Used Languages:</h2>
-								<ul>
-									{languageUsage.map((language) => (
-										<li key={language.language}>
-											{language.language}: {language.count} (
-											{language.percentage.toFixed(2)}%)
-											<div
-												className={styles.value}
-												style={{ width: `${language.percentage.toFixed(2)}%` }}
-											></div>
-										</li>
-									))}
-								</ul>
+								{profile.message !== undefined && (
+									<React.Fragment>
+										<div className={styles.api_limit}>
+											<Alert text={`${profile.message}`} />
+											<Link href={profile.documentation_url} target="_blank">
+												GitHub documentation.
+											</Link>
+										</div>
+									</React.Fragment>
+								)}
+
+								{profile && profile.message === undefined && (
+									<React.Fragment>
+										<Image
+											src={`${profile?.avatar_url}`}
+											width={150}
+											height={150}
+											alt="profile image"
+											priority={true}
+										/>
+										<div className={styles.user_info}>
+											<h1>{profile?.name}</h1>
+											<span className={styles.description}>{profile?.bio}</span>
+											<div className={styles.user_info_rows}>
+												<div className={styles.row}>
+													<MapPinIcon width={16} height={16} />
+													{profile?.location}
+												</div>
+												<div className={styles.row}>
+													<UsersIcon width={16} height={16} />
+													<Link href={profile?.followers_url}>
+														{profile?.followers} followers
+													</Link>
+													<Link href={profile?.following_url}>
+														{profile?.following} following
+													</Link>
+												</div>
+											</div>
+											<Link
+												className={styles.github_link}
+												href={profile?.html_url}
+												target="_blank"
+											>
+												GitHub
+												<ArrowTopRightOnSquareIcon width={14} height={14} />
+											</Link>
+										</div>
+									</React.Fragment>
+								)}
 							</div>
 						</div>
 					</div>
@@ -138,42 +128,13 @@ export const getServerSideProps = async (
 	const { username } = context.params as { username: string }; // Type assertion
 
 	try {
-		// Fetch profile information, most used languages, and activity using the GitHub API
 		const response = await fetch(`https://api.github.com/users/${username}`);
 		const profileData = await response.json();
-
-		const reposResponse = await fetch(profileData.repos_url);
-		const reposData: Repo[] = await reposResponse.json();
-
-		const totalRepoCount = reposData.length;
-
-		const languageCount: { [key: string]: number } = {};
-
-		reposData.forEach((repo) => {
-			const { language } = repo;
-
-			if (language) {
-				if (languageCount[language]) {
-					languageCount[language] += 1;
-				} else {
-					languageCount[language] = 1;
-				}
-			}
-		});
-
-		const sortedLanguages = Object.entries(languageCount)
-			.map(([language, count]) => ({
-				language,
-				count,
-				percentage: (count / totalRepoCount) * 100,
-			}))
-			.sort((a, b) => b.count - a.count);
 
 		return {
 			props: {
 				session,
 				profileData,
-				languageUsage: sortedLanguages,
 			},
 		};
 	} catch (error) {
@@ -182,7 +143,6 @@ export const getServerSideProps = async (
 			props: {
 				session,
 				profileData: null,
-				languageUsage: [],
 			},
 		};
 	}
