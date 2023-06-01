0b9587a6f8d431b95d826c643a5129e40894f33d
KoffeMeow <guyden999@mail.ru>
2023-06-01 22:42:07 +0300

    + status useSession()

--- components/_Templates/AuthorizationPopup/index.tsx
+++ components/_Templates/AuthorizationPopup/index.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
+
 import Image from 'next/image';
 
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+
+import Alert from '@/components/Alert';
 import Popup from '@/components/Popup';
 
 import styles from './index.module.scss';
@@ -15,28 +20,60 @@ const AuthorizationPopup: React.FC<AuthorizationPopupProps> = ({
 	title,
 	buttonContent,
 }) => {
+	const { status } = useSession();
+
 	return (
 		<Popup title={title} buttonContent={buttonContent}>
-			<div className={styles.buttons}>
-				<button onClick={() => signIn('github')} id={styles.github}>
-					Continue with GitHub
-					<Image
-						src="/icons/services/github.svg"
-						width={16}
-						height={16}
-						alt="github"
-					/>
-				</button>
-				<button onClick={() => signIn('gitlab')} id={styles.gitlab}>
-					Continue with GitLab
-					<Image
-						src="/icons/services/gitlab.svg"
-						width={16}
-						height={16}
-						alt="gitlab"
-					/>
-				</button>
-			</div>
+			{status === 'unauthenticated' && (
+				<div className={styles.buttons}>
+					<button onClick={() => signIn('github')} id={styles.github}>
+						<div className={styles.button_content}>
+							Continue with GitHub
+							<Image
+								src="/icons/services/github.svg"
+								width={16}
+								height={16}
+								alt="github"
+							/>
+						</div>
+						<ArrowRightIcon width={16} height={16} strokeWidth={2} />
+					</button>
+					<button onClick={() => signIn('gitlab')} id={styles.gitlab}>
+						<div className={styles.button_content}>
+							Continue with GitLab
+							<Image
+								src="/icons/services/gitlab.svg"
+								width={16}
+								height={16}
+								alt="gitlab"
+							/>
+						</div>
+						<ArrowRightIcon width={16} height={16} strokeWidth={2} />
+					</button>
+				</div>
+			)}
+
+			{status === 'authenticated' && (
+				<React.Fragment>
+					<Alert text="You are already logged in!" />
+					<div className={styles.buttons} style={{ marginTop: '12px' }}>
+						<button
+							onClick={() => signIn('github')}
+							id={styles.github}
+							disabled={true}
+						>
+							Continue with GitHub
+						</button>
+						<button
+							onClick={() => signIn('gitlab')}
+							id={styles.gitlab}
+							disabled={true}
+						>
+							Continue with GitLab
+						</button>
+					</div>
+				</React.Fragment>
+			)}
 		</Popup>
 	);
 };
