aab7569415e1598b8a456b4ffb8cfd3ac62e282c
KoffeMeow <guyden999@mail.ru>
2023-06-03 23:54:33 +0300

    i18n init

---
+++ next-i18next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en', 'ru'],
+		localeDetection: true,
+	},
+	react: {
+		useSuspense: false, // Включение отложенной загрузки переводов
+	},
+};
--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-/** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
+
 const nextConfig = {
 	experimental: {
 		appDir: true,
 		typedRoutes: true,
 	},
+
+	reactStrictMode: true,
 
 	images: {
 		remotePatterns: [
@@ -19,6 +22,8 @@ const nextConfig = {
 			},
 		],
 	},
+
+	i18n,
 };
 
 module.exports = nextConfig;
