a83b5f233ca0e993c751e4c8681164adef558a6f
KoffeMeow <guyden999@mail.ru>
2023-06-04 19:07:46 +0300

    + localePath

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,6 +3,7 @@ module.exports = {
 		defaultLocale: 'en',
 		locales: ['en', 'ru'],
 		localeDetection: true,
+		localePath: path.resolve('./public/locales'),
 	},
 	react: {
 		useSuspense: false, // Включение отложенной загрузки переводов
