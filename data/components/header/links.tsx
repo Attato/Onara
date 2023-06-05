7fdd47e32b67107006d1ede47833cbf8c964c160
KoffeMeow <guyden999@mail.ru>
2023-06-05 10:47:53 +0300

    link update

--- data/components/header/links.tsx
+++ data/components/header/links.tsx
@@ -2,7 +2,7 @@ export const links = [
 	{ href: '/changelog', label: 'Changelog', target: '_self' },
 	{ href: '/docs/introduction', label: 'Docs', target: '_self' },
 	{
-		href: 'https://www.patreon.com/Attato',
+		href: 'https://opencollective.com/onara',
 		label: 'Support',
 		target: '_blank',
 	},
