c13604108a7969fd58ff43cae409d3b96cbe6793
KoffeMeow <guyden999@mail.ru>
2023-06-05 09:46:18 +0300

    page under constuction

--- pages/[username]/groups/index.tsx
+++ pages/[username]/groups/index.tsx
@@ -6,15 +6,45 @@ import { getSession } from 'next-auth/react';
 
 import { ProfileProps } from '..';
 import Sidebar from '@/components/Sidebar';
+import Alert from '@/components/Alert';
 
 import styles from './index.module.scss';
 
 const Groups: NextPage<ProfileProps> = ({ profileData }) => {
 	const [profile, setProfile] = useState<any>(profileData);
+	const [groupName, setGroupName] = useState('');
+	const [friends, setFriends] = useState<string[]>([]);
+	const [selectedFriends, setSelectedFriends] = useState<string[]>([]);
+	const [groups, setGroups] = useState<any[]>([]);
 
 	useEffect(() => {
 		setProfile(profileData);
 	}, [profileData]);
+
+	const handleGroupNameChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setGroupName(event.target.value);
+	};
+
+	const handleFriendSelection = (friend: string) => {
+		setSelectedFriends((prevSelectedFriends) =>
+			prevSelectedFriends.includes(friend)
+				? prevSelectedFriends.filter((f) => f !== friend)
+				: [...prevSelectedFriends, friend]
+		);
+	};
+
+	const handleCreateGroup = () => {
+		const newGroup = {
+			name: groupName,
+			friends: selectedFriends,
+		};
+
+		setGroups((prevGroups) => [...prevGroups, newGroup]);
+		setGroupName('');
+		setSelectedFriends([]);
+	};
 
 	return (
 		<React.Fragment>
@@ -31,7 +61,43 @@ const Groups: NextPage<ProfileProps> = ({ profileData }) => {
 			<div className={styles.groups}>
 				<div className={styles.groups_content}>
 					<Sidebar username={profile?.name} />
-					<h1>Groups</h1>
+					<div className={styles.groups_list}>
+						<div className={styles.create}>
+							<input
+								type="text"
+								placeholder="Group Name"
+								value={groupName}
+								onChange={handleGroupNameChange}
+							/>
+							<button onClick={handleCreateGroup}>Create Group</button>
+						</div>
+						{/* <h3>Select Friends:</h3>
+						{friends.map((friend) => (
+							<div key={friend}>
+								<label>
+									<input
+										type="checkbox"
+										checked={selectedFriends.includes(friend)}
+										onChange={() => handleFriendSelection(friend)}
+									/>
+									{friend}
+								</label>
+							</div>
+						))}
+						<h2>Your Groups</h2>
+						{groups.map((group) => (
+							<div key={group.name}>
+								<h3>{group.name}</h3>
+								<ul>
+									{group.friends.map((friend: string) => (
+										<li key={friend}>{friend}</li>
+									))}
+								</ul>
+							</div>
+						))} */}
+
+						<Alert text="Under construction..." />
+					</div>
 				</div>
 			</div>
 		</React.Fragment>
@@ -57,10 +123,14 @@ export const getServerSideProps = async (
 		const response = await fetch(`https://api.github.com/users/${username}`);
 		const profileData = await response.json();
 
+		// Fetch friends data from an API or other source
+		const friendsData = ['Friend 1', 'Friend 2', 'Friend 3'];
+
 		return {
 			props: {
 				session,
 				profileData,
+				friendsData,
 			},
 		};
 	} catch (error) {
@@ -69,6 +139,7 @@ export const getServerSideProps = async (
 			props: {
 				session,
 				profileData: null,
+				friendsData: [],
 			},
 		};
 	}
