cac2c4538f22baa1e775a851cdb7eff7ef540911
KoffeMeow <guyden999@mail.ru>
2023-06-15 23:59:51 +0300

    + Repository model

--- prisma/migrations/20230615181751_repositories_add/migration.sql
+++ prisma/migrations/20230615181751_repositories_add/migration.sql
@@ -32,19 +32,40 @@ CREATE TABLE "Session" (
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "emailVerified" TEXT,
+    "email_verified" TEXT,
     "name" TEXT,
     "image" TEXT,
-    "htmlUrl" TEXT,
+    "html_url" TEXT,
     "bio" TEXT,
     "location" TEXT,
-    "createdAt" TEXT,
-    "updatedAt" TEXT,
+    "created_at" TEXT,
+    "updated_at" TEXT,
     "followers" INTEGER,
     "following" INTEGER,
-    "starredRepos" INTEGER,
+    "starred_repos" INTEGER,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Repository" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "owner" TEXT NOT NULL,
+    "html_url" TEXT NOT NULL,
+    "description" TEXT,
+    "created_at" TEXT NOT NULL,
+    "updated_at" TEXT NOT NULL,
+    "git_url" TEXT NOT NULL,
+    "ssh_url" TEXT NOT NULL,
+    "clone_url" TEXT NOT NULL,
+    "homepage" TEXT,
+    "stargazers_count" INTEGER NOT NULL,
+    "watchers_count" INTEGER NOT NULL,
+    "language" TEXT,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "Repository_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -56,8 +77,14 @@ CREATE UNIQUE INDEX "Session_session_token_key" ON "Session"("session_token");
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Repository_name_user_id_key" ON "Repository"("name", "user_id");
+
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Repository" ADD CONSTRAINT "Repository_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
