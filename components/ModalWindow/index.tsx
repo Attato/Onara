7d4e8db13bfd8c8276cadc5f8802eb1892f0b397
KoffeMeow <guyden999@mail.ru>
2023-06-16 23:55:27 +0300

    init

---
+++ components/ModalWindow/index.tsx
@@ -0,0 +1,85 @@
+import React, { useState, ReactNode, FC } from 'react';
+import { Transition, Dialog } from '@headlessui/react';
+
+const ModalWindow: FC = ({}) => {
+	let [isModalOpen, setIsModalOpen] = useState(false);
+
+	function closeModal() {
+		setIsModalOpen(false);
+	}
+
+	function openModal() {
+		setIsModalOpen(true);
+	}
+
+	return (
+		<React.Fragment>
+			<div className="w-full max-w-[125px] flex items-center justify-center">
+				<button
+					type="button"
+					onClick={openModal}
+					className="w-full h-full rounded-lg bg-surface100 border border-border dark:border-borderDark dark:bg-surface100Dark text-sm text-colorSecondary dark:text-colorSecondaryDark"
+				>
+					Add friends
+				</button>
+			</div>
+
+			<Transition appear show={isModalOpen} as={React.Fragment}>
+				<Dialog as="div" className="relative z-10" onClose={closeModal}>
+					<Transition.Child
+						as={React.Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div className="fixed inset-0 bg-black bg-opacity-25" />
+					</Transition.Child>
+
+					<div className="fixed inset-0 overflow-y-auto">
+						<div className="flex min-h-full items-center justify-center p-4 text-center">
+							<Transition.Child
+								as={React.Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95"
+							>
+								<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+									<Dialog.Title
+										as="h3"
+										className="text-lg font-medium leading-6 text-gray-900"
+									>
+										Payment successful
+									</Dialog.Title>
+									<div className="mt-2">
+										<p className="text-sm text-gray-500">
+											Your payment has been successfully submitted. We’ve sent
+											you an email with all of the details of your order.
+										</p>
+									</div>
+
+									<div className="mt-4">
+										<button
+											type="button"
+											className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+											onClick={closeModal}
+										>
+											Got it, thanks!
+										</button>
+									</div>
+								</Dialog.Panel>
+							</Transition.Child>
+						</div>
+					</div>
+				</Dialog>
+			</Transition>
+		</React.Fragment>
+	);
+};
+
+export default ModalWindow;
