6a8b6c5bf39d6e5754e0855a06f1b9fa0eccf162
KoffeMeow <guyden999@mail.ru>
2023-06-17 11:10:22 +0300

    relocate ThemeChanger and LanguageChager logic to other files

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,132 +1,14 @@
-import React, { useState, useEffect, FC } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
 import Link from 'next/link';
-import { useTranslation } from 'next-i18next';
-import { Listbox } from '@headlessui/react';
-import { ChevronUpDownIcon, CheckIcon } from '@heroicons/react/24/outline';
-import { useTheme } from 'next-themes';
-import {
-	SunIcon,
-	MoonIcon,
-	ComputerDesktopIcon,
-} from '@heroicons/react/24/solid';
+import ThemeChanger from '@/components/_ThemeChanger';
+import LanguageChanger from '@/components/_LanguageChanger';
 
-const Footer: FC = () => {
-	const { theme, setTheme } = useTheme();
-	const [mounted, setMounted] = useState<boolean>(false);
-
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-
-	const themeOptions = [
-		{
-			theme: 'light',
-			icon: <SunIcon className="w-5 h-5 flex-1" />,
-			title: 'Light Theme',
-		},
-		{
-			theme: 'dark',
-			icon: <MoonIcon className="w-5 h-5 flex-1" />,
-			title: 'Dark Theme',
-		},
-		{
-			theme: 'system',
-			icon: <ComputerDesktopIcon className="w-5 h-5 flex-1" />,
-			title: 'System Theme',
-		},
-	];
-
-	const renderThemeChanger = () => {
-		if (!mounted) return null;
-
-		return (
-			<div className="flex-1">
-				<div className="ml-auto flex w-fit space-x-2 border border-border dark:border-borderDark bg-surface100 dark:bg-surface100Dark p-1 rounded-3xl">
-					{themeOptions.map((option) => (
-						<button
-							key={option.theme}
-							className={`${
-								theme === option.theme && 'bg-accent text-colorPrimaryDark'
-							} p-1 rounded-[50%] border-2 border-transparent transition-all hover:border-accent`}
-							onClick={() => setTheme(option.theme)}
-							title={option.title}
-						>
-							{option.icon}
-						</button>
-					))}
-				</div>
-			</div>
-		);
-	};
-
-	const changeLocale = (locale: string) => {
-		document.cookie = `NEXT_LOCALE=${locale}`;
-	};
-
-	const { t } = useTranslation();
-
-	const router = useRouter();
-
-	const { locales, locale, query, pathname } = router;
-
-	const languageOptions = [
-		{
-			locale: 'en',
-			label: t('common:languages.english'),
-		},
-		{
-			locale: 'ru',
-			label: t('common:languages.russian'),
-		},
-	];
-
+const Footer = () => {
 	return (
 		<footer className="p-6 bg-background dark:bg-backgroundDark border-t border-border dark:border-borderDark select-none">
 			<div className="flex flex-col gap-4 max-w-5xl m-auto text-colorPrimary dark:text-colorPrimaryDark">
 				<div className="flex justify-between">
-					<Listbox value={locales}>
-						{({ open }) => (
-							<div className="relative h-fit w-full max-w-[220px] mr-4">
-								<Listbox.Button
-									className={`${
-										open && 'ring-2 ring-accent dark:ring-accent'
-									} flex items-center justify-between w-full rounded-md  py-1.5 px-3 bg-surface100 dark:bg-surface100Dark text-colorPrimary dark:text-colorPrimaryDark shadow-sm ring-1 ring-inset ring-border dark:ring-borderDark sm:text-sm sm:leading-6`}
-								>
-									<span className="flex items-center">
-										{locale === 'en' && <>{t('common:languages.english')}</>}
-										{locale === 'ru' && <>{t('common:languages.russian')}</>}
-									</span>
-									<ChevronUpDownIcon className="h-5 w-5" aria-hidden="true" />
-								</Listbox.Button>
-
-								{open && (
-									<Listbox.Options className="absolute left-0 rounded-md mt-1 w-full py-1 border border-border dark:border-borderDark bg-surface100 dark:bg-surface100Dark">
-										{languageOptions.map((option) => (
-											<Link
-												key={option.locale}
-												href={{ pathname, query }}
-												locale={option.locale}
-												onClick={() => changeLocale(option.locale)}
-											>
-												<Listbox.Option
-													value={option.locale}
-													className={`${
-														option.locale === locale && 'font-medium'
-													} flex text-sm items-center justify-between px-4 py-2 cursor-pointer hover:bg-accent hover:text-slate-100 transition-all`}
-												>
-													{option.label}
-													{option.locale === locale && (
-														<CheckIcon width={16} height={16} strokeWidth={2} />
-													)}
-												</Listbox.Option>
-											</Link>
-										))}
-									</Listbox.Options>
-								)}
-							</div>
-						)}
-					</Listbox>
+					<LanguageChanger />
 
 					<div className="max-w-[220px] w-full flex flex-col mx-4">
 						<h3 className="font-medium mb-4">Product</h3>
@@ -173,7 +55,7 @@ const Footer: FC = () => {
 							ONARA
 						</Link>
 					</div>
-					{renderThemeChanger()}
+					<ThemeChanger />
 				</div>
 			</div>
 		</footer>
