38f1e371b9c3b3b088243afa5bb47275bc17a041
KoffeMeow <guyden999@mail.ru>
2023-06-18 21:24:41 +0300

    page update

--- pages/[username]/friends/index.tsx
+++ pages/[username]/friends/index.tsx
@@ -3,12 +3,8 @@ import React, { useState } from 'react';
 import { NextPage, GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
-import { useTheme } from 'next-themes';
 
 import { getSession } from 'next-auth/react';
-
-import { MapPinIcon } from '@heroicons/react/24/outline';
 
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { fetchProfileData } from '@/lib/profile';
@@ -19,15 +15,13 @@ import Sidebar from '@/components/Sidebar';
 import Tabs from '@/components/Tabs';
 
 const Friends: NextPage<any> = ({ profileData }) => {
-	const { theme } = useTheme();
-
 	const [searchQuery, setSearchQuery] = useState('');
 
-	const filteredFollowers = profileData?.followers.filter((friend: any) =>
-		friend.login.toLowerCase().includes(searchQuery.toLowerCase())
-	);
+	// const filteredFollowers = profileData?.followers.filter((friend: any) =>
+	// 	friend.login.toLowerCase().includes(searchQuery.toLowerCase())
+	// );
 
-	const isUserNotFound = filteredFollowers.length === 0;
+	// const isUserNotFound = filteredFollowers.length === 0;
 
 	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSearchQuery(event.target.value);
@@ -42,6 +36,24 @@ const Friends: NextPage<any> = ({ profileData }) => {
 	function openModal() {
 		setIsModalOpen(true);
 	}
+
+	const [selectedFriends, setSelectedFriends] = useState<any[]>([]);
+
+	const handleFriendClick = (friend: any) => {
+		setSelectedFriends((prevSelectedFriends: any[]) => {
+			const isFriendSelected = prevSelectedFriends.some(
+				(selectedFriend) => selectedFriend.id === friend.id
+			);
+			if (isFriendSelected) {
+				return prevSelectedFriends.filter(
+					(selectedFriend) => selectedFriend.id !== friend.id
+				);
+			}
+			return [...prevSelectedFriends, friend];
+		});
+	};
+
+	console.log(selectedFriends);
 
 	return (
 		<React.Fragment>
@@ -104,29 +116,124 @@ const Friends: NextPage<any> = ({ profileData }) => {
 												leaveFrom="opacity-100 scale-100"
 												leaveTo="opacity-0 scale-95"
 											>
-												<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-													<Dialog.Title
-														as="h3"
-														className="text-lg font-medium leading-6 text-gray-900"
-													>
-														Payment successful
-													</Dialog.Title>
-													<div className="mt-2">
-														<p className="text-sm text-gray-500">
-															Your payment has been successfully submitted.
-															We’ve sent you an email with all of the details of
-															your order.
-														</p>
-													</div>
+												<Dialog.Panel className="w-full max-w-2xl transform rounded-lg bg-background dark:bg-backgroundDark p-6 text-left align-middle shadow-xl transition-all">
+													<h2 className="mb-4 text-xl font-medium">
+														Add friends
+													</h2>
 
-													<div className="mt-4">
-														<button
-															type="button"
-															className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-															onClick={closeModal}
-														>
-															Got it, thanks!
-														</button>
+													<div className="flex flex-col">
+														<div className="flex flex-col justify-start uppercase text-colorSecondary dark:text-colorSecondaryDark font-semibold text-xs h-[512px] overflow-auto gap-2">
+															<h3>Following</h3>
+															<div className="flex flex-col gap-2 border-l border-l-border dark:border-l-borderDark">
+																{profileData?.following.map((follow: any) => {
+																	const isFriendSelected =
+																		selectedFriends.includes(follow.id);
+
+																	return (
+																		<div
+																			key={follow.id}
+																			className={`flex items-center select-none normal-case rounded-md bg-surface100 dark:bg-surface100Dark hover:bg-surface200 hover:dark:bg-surface200Dark transition-all mx-4  ${
+																				isFriendSelected
+																					? 'bg-surface200 dark:bg-surface200Dark'
+																					: ''
+																			}`}
+																		>
+																			<label
+																				htmlFor={`friend-${follow.id}`}
+																				className="flex w-full gap-3 items-center px-3 py-2 cursor-pointer"
+																			>
+																				{/* <Image
+																					src={follow.image}
+																					width={32}
+																					height={32}
+																					alt={`${follow.login} avatar`}
+																					className="rounded-[50%] h-fit"
+																				/> */}
+
+																				<div className="flex flex-col w-full">
+																					<h3 className="text-colorPrimary dark:text-colorPrimaryDark text-base font-medium">
+																						{follow.login}
+																					</h3>
+
+																					<span className="flex gap-1 items-center text-xs">
+																						<span>@{follow.id} </span>
+																					</span>
+																				</div>
+
+																				<input
+																					id={`friend-${follow.id}`}
+																					type="checkbox"
+																					checked={isFriendSelected}
+																					onChange={() =>
+																						handleFriendClick(follow)
+																					}
+																				/>
+																			</label>
+																		</div>
+																	);
+																})}
+															</div>
+
+															<h3>Followers</h3>
+															<div className="flex flex-col gap-2 border-l border-l-border dark:border-l-borderDark">
+																{profileData?.followers.map((follower: any) => {
+																	const isFriendSelected =
+																		selectedFriends.includes(follower.id);
+
+																	return (
+																		<div
+																			key={follower.id}
+																			className={`flex items-center select-none normal-case rounded-md bg-surface100 dark:bg-surface100Dark hover:bg-surface200 hover:dark:bg-surface200Dark transition-all mx-4 ${
+																				isFriendSelected
+																					? 'bg-surface200 dark:bg-surface200Dark'
+																					: ''
+																			}`}
+																		>
+																			<label
+																				htmlFor={`friend-${follower.id}`}
+																				className="flex w-full gap-3 items-center px-3 py-2 cursor-pointer"
+																			>
+																				{/* <Image
+																					src={follower.image}
+																					width={32}
+																					height={32}
+																					alt={`${follower.login} avatar`}
+																					className="rounded-[50%] h-fit"
+																				/> */}
+
+																				<div className="flex flex-col w-full">
+																					<h3 className="text-colorPrimary dark:text-colorPrimaryDark text-base font-medium">
+																						{follower.login}
+																					</h3>
+
+																					<span className="flex gap-1 items-center text-xs">
+																						<span>@{follower.id} </span>
+																					</span>
+																				</div>
+
+																				<input
+																					id={`friend-${follower.id}`}
+																					type="checkbox"
+																					checked={isFriendSelected}
+																					onChange={() =>
+																						handleFriendClick(follower)
+																					}
+																				/>
+																			</label>
+																		</div>
+																	);
+																})}
+															</div>
+														</div>
+														<div className="flex items-center justify-between w-full font-medium text-sm">
+															<span>Selected: {selectedFriends.length}</span>
+															<button
+																// onClick={handleAddFriends}
+																className="bg-accent hover:bg-indigo-500 transition-all text-colorPrimaryDark px-3 w-full max-w-[160px] px py-2 rounded-md"
+															>
+																Add +
+															</button>
+														</div>
 													</div>
 												</Dialog.Panel>
 											</Transition.Child>
@@ -137,7 +244,7 @@ const Friends: NextPage<any> = ({ profileData }) => {
 						</div>
 					</div>
 
-					{isUserNotFound ? (
+					{/* {isUserNotFound ? (
 						<div className="flex flex-col h-full items-center justify-center">
 							<Image
 								src={
@@ -197,7 +304,7 @@ const Friends: NextPage<any> = ({ profileData }) => {
 								);
 							})}
 						</React.Fragment>
-					)}
+					)} */}
 				</div>
 			</div>
 		</React.Fragment>
