ede4d9f8c58ede83bb9ad9910cf5c8ef7e66c689
KoffeMeow <guyden999@mail.ru>
2023-06-19 17:31:43 +0300

    prisma update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,10 +4,13 @@ declare global {
 	var prisma: PrismaClient;
 }
 
-if (!global.prisma) {
-	global.prisma = new PrismaClient();
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient();
+} else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient();
+	}
+	prisma = global.prisma;
 }
-
-const prisma = global.prisma;
 
 export default prisma;
